2954a324ee2446f903c1ef92506c47278a144e9b
ThomasBaynes <47601530+lost0000@users.noreply.github.com>
2021-04-17 12:54:41 +0930

    Posts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,7 +15,8 @@ class DashboardController extends Controller
 
     public function index() 
     {
-        
+        dd(auth() -> user() -> posts);
+
         return view('dashboard');
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() 
     {
-        
-        return view('posts.index');
+        // $posts = Post::get(); //laravel coll
+         $posts = Post::paginate(2); //just gets amount specified instead of all //paginate will only work with tailwind but may need custom with bootstrap etc.
+        return view('posts.index', [
+            'posts' => $posts       //gets all posts and puts them into an array on the posts view. Can use for cart and also for main shop
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        auth() -> user() -> posts() -> create([
+            'body' => $request -> body
+        ]);
+
+        return back();
     }
 }
